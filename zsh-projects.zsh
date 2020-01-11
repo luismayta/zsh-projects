469e78d4138fbb992c6f5a5775117fcb9ef61ad8
@slovacus <slovacus@gmail.com>
2020-01-11 00:31:34 -0500

    (#3) chore: Implement multiple project for cookiecutter

--- zsh-projects.zsh
+++ zsh-projects.zsh
@@ -12,8 +12,17 @@ projects_package_name='projects'
 PROJECTS_ROOT_DIR=$(dirname "$0")
 PROJECTS_SRC_DIR="${PROJECTS_ROOT_DIR}"/src
 
+function cookiecutter::install {
+    message_info "Installing cookiecutter for ${projects_package_name}"
+    if type -p pip > /dev/null; then
+        pip install --user cookiecutter
+        message_success "Installed cookiecutter for ${projects_package_name}"
+    fi
+}
+
 function projects::list {
-    cat "${PROJECTS_SRC_DIR}"/data.json | jq '.projects[] | "\(.name) | \(.type) | \(.description) | \(.command)"'
+    # shellcheck disable=SC2002
+    cat "${PROJECTS_SRC_DIR}"/data.json | jq '.projects[] | "\(.name) | \(.type) | \(.description) | \(.repository)"'
 }
 
 function projects::find {
@@ -24,5 +33,7 @@ function projects::find {
                     | perl -pe 'chomp' \
                     | sed 's/\"//g'
                 )
-    echo "${command_value}"
+    cookiecutter "${command_value}"
 }
+
+if ! type -p cookiecutter > /dev/null; then cookiecutter::install; fi
